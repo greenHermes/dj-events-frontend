dcd073fbfa312f8500d3a4cb4f380f3d5c1ec0cc
Nicholas Karsant <nkarsant@gmail.com>
2021-05-23 15:20:22 -0700

    sstup API and dispaly data

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,31 @@
+import Link from 'next/link'
 import Layout from '@/components/Layout'
+import EventItem from '@/components/EventItem'
+import { API_URL } from '@/config/index'
 
-export default function EventsPage() {
+export default function EventsPage({ events }) {
+  // console.log(events.length)
   return (
     <Layout>
-    <h1>My Events</h1>
+      <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`)
+  console.log(res.json)
+  // const events = await res.json()
+
+  const events = [{id: 1, name: "Hardcoded Name of Event", time: "3:00", date: "April 20th, 2020", data: "data"}]
+  return {
+    props: {events},
+    revalidate: 1
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,44 @@
-import Link from 'next/Link'
+import Link from 'next/link'
 import Layout from '@/components/Layout'
+import EventItem from '@/components/EventItem'
+import { API_URL } from '@/config/index'
 
 
-export default function HomePage() {
+export default function HomePage({events}) {
+  console.log(events.length);
   return (
     <Layout>
 
 
-      <h1>Home</h1>
-      <Link href='/about'>About</Link>
+      <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3> No events to show</h3>}
 
 
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
 
-    </Layout>
+      {events.length > 0 && (
+        <Link href='/events'>
+          <a className='btn-secondary'>View All Events</a>
+        </Link>
+      )}
+
+    </Layout> 
   )
+}
+
+export async function getStaticProps(){
+  const res = await fetch(`${API_URL}/api/events`)
+
+  // console.log(res)
+  // const events = await res.response.json()
+
+
+  const events = [{id: 1, name: "Hardcoded Name of Event", time: "3:00", date: "April 20th, 2020", data: "data"}]
+  return {
+    props: {events},
+    revalidate: 1
+  }
+
 }
